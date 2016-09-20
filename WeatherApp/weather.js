532b6007bf181c9be26807e3ed6c8cae55d0413a
landon228 <landon.mc52@gmail.com>
2016-09-20 16:37:43 -0400

    l

--- WeatherApp/weather.js
+++ WeatherApp/weather.js
@@ -1,40 +1,40 @@
 $('document').ready(function(){
 	var latitude;
 	var longitude;
-	var apiUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=88d7747abc290c8a803e9c34ae064b02&units=metric";
-	$.getJSON("http://ip-api.com/json",function(data2)
+	$.getJSON("http://ip-api.com/json", function(data2){
 	latitude = data2.lat;
 	longitude = data2.lon;
+	var apiUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=88d7747abc290c8a803e9c34ae064b02&units=metric";
+
+	$.getJSON(apiUrl, function(data){
+		var tempC;
+		var tempF;
+		var weatherCondition = data.weather[0].description;
+		var kelvinTemp = data.main.temp;
+		var city = data.name;
+		var wind = data.wind.speed;
+		var switchTemp = true;
+
+		tempF = (kelvinTemp * (9 / 5) - 459.67).toFixed(0);
+		tempC = (kelvinTemp - 273).toFixed(0);
+		wind = (2.237 * ( wind )).toFixed(0);
+
+		$("#city").html(city);
+		$("#tempF").html(tempF + " &#8457;");
+		$("#condition").html(weatherCondition);
+		$("#wind").html(wind + " MPH");
+
+		$("tempF").click(function(){
+			if(switchTemp === false){
+				$("tempF").html(tempF + " &#8457;");
+				switchTemp = true;
+			}else{
+				$("tempF").html(tempC + " &#8451;");
+				switchTemp = false;
+			}
+		});
+
+	});
 
 });
-		$.getJSON(apiUrl, function(data){
-			var weatherCondition = data.weather[0].description;
-			var kelvinTemp = data.main.temp;
-			var switchTemp = true;
-			tempF = (kelvinTemp * (9 / 5) - 459.67).toFixed(0);
-			tempC = (kelvinTemp - 273).toFixed(0);
-			var city = data.name
-			var wind = data.wind.speed;
-			wind = (2.237 * ( wind )).toFixed(0);
-
-
-
-			$("#city").html(city);
-			$("#tempF").html(tempF + " &#8457;");
-			$("#condition").html(weatherCondition);
-			$("#wind").html(wind + " MPH");
-
-			$("tempF").click(function(){
-				if(switchTemp === false){
-					$("tempF").html(tempF + " &#8457;");
-					switchTemp = true;
-				}else{
-					$("tempF").html(tempC + " &#8451;");
-					switchTemp = false;
-				}
-			});
-
-	});
-
-
-	});
+});
