2af6b093649796be0c4b1ec293285abf5bd29d58
lamckee <l_mckee0902@email.campbell.edu>
2017-04-18 15:54:42 -0400

    Comments and cleanup
    added comments, removed footer, updated colors for background and quote
    div

--- QuoteGeneratorApp/quotescript.js
+++ QuoteGeneratorApp/quotescript.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
   newQuote();
 });
 
+//quotes are recieved from forismatic api via the url set as a variable below.
+
 function newQuote(){
 	var apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?'
+
+//function will receive data from forismatic, trim it, and store into variables
 
 	$.getJSON(apiURL).done(function (data) {
       var quote = data.quoteText.trim(),
@@ -11,15 +15,18 @@ function newQuote(){
        tweetText = (quote + ' ' + author),
        twitterURL = 'https://twitter.com/intent/tweet?text=' + tweetText;
 
- $('#quote').text(quote);
- $('#author').text(author);
- $('#tweetButton').attr('href', twitterURL);
+       //inputing variable into the HTML
+       $('#quote').text(quote);
+       $('#author').text(author);
+       $('#tweetButton').attr('href', twitterURL);
 
-			var colors = ["#f94e3f","#e77e4d","#e3632d","#6f2108"];
+      //colors of the box will change randomly between 4 colors on every new quote
+			var colors = ["#a5dff9","#60c5ba","#ef5285","#feee7d"];
 			var rand = Math.floor(Math.random()*colors.length);
  $('#quoteBox').css("background-color", colors[rand]);
 
-			var backgroundColors = ["#a79c8e","#f8ecc9","#f1bbba",""];                
+      //colors of the background will change randomly between 4 colors on every new quote
+			var backgroundColors = ["#a79c8e","#f8ecc9","#D8E6E7","#EFFFE9"];
 			var rand2 = Math.floor(Math.random()*backgroundColors.length);
  $('body').css("background-color", backgroundColors[rand]);
 
